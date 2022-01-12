60cc19a813b066fdd656950b9c73f43574654dc4
uom <uom.2000@gmail.com>
2022-01-13 04:09:03 +0700

    NumericParseResult improv

--- NavfertyExcelAddIn/ParseNumerics/NumericParseResult.cs
+++ NavfertyExcelAddIn/ParseNumerics/NumericParseResult.cs
@@ -9,75 +9,26 @@ namespace NavfertyExcelAddIn.ParseNumerics
 {
 	public class NumericParseResult
 	{
-		private static readonly string _currencySystem = CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol;
-		private static readonly string _currencyRu = CultureInfo.GetCultureInfo("ru-RU").NumberFormat.CurrencySymbol;
+		private static readonly string currencySymbolFromOSUserLocale = CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol;
+		private static readonly string currencySymbolRu = CultureInfo.GetCultureInfo("ru").NumberFormat.CurrencySymbol;
 
 		public readonly decimal? ConvertedValue = null;
 		public readonly string Currency = "";
-		public readonly bool IsMoney = false;
 
-		public NumericParseResult(decimal? value, string curr = "")
+		public NumericParseResult(decimal? value, string curr = null)
 		{
 			ConvertedValue = value;
-			if (null != curr) Currency = curr;
-			IsMoney = (null != Currency && !string.IsNullOrEmpty(Currency));
+			Currency = curr;
 		}
+
+		public bool IsMoney
+			=> !string.IsNullOrEmpty(Currency);
 
 		public bool IsCurrencyFromCurrentCulture()
-			=> (_currencySystem == Currency);
+			=> (currencySymbolFromOSUserLocale == Currency);
 
 		public bool IsCurrencyFromRU()
-			=> (_currencyRu == Currency);
-
-		/// <summary>This code is sample from internet - WAS NOT CHECKED!!!</summary>
-		private static string GetExcelCurrencyFormatString(CultureInfo culture = null)
-		{
-			if (culture == null) culture = CultureInfo.CurrentCulture;
-
-			//we'll use string.Format later to replace {0} with the currency symbol
-			//and {1} with the number format
-			string[] negativePatternStrings = {
-				"({0}{1})",
-				"-{0}{1}",
-				"{0}-{1}",
-				"{0}{1}-",
-				"({1}{0})",
-				"-{1}{0}",
-				"{1}-{0}",
-				"{1}{0}-",
-				"-{1} {0}",
-				"-{0} {1}",
-				"{1} {0}-",
-				"{0} {1}-",
-				"{0} -{1}",
-				"{1}- {0}",
-				"({0} {1})",
-				"({1} {0})" };
-			string[] positivePatternStrings = {
-				"{0}{1}",
-				"{1}{0}",
-				"{0} {1}",
-				"{1}{0}" };
-
-			var numberFormat = culture.NumberFormat;
-
-			//Generate 0's to fill in the format after the decimal place
-			var decimalPlaces = new string('0', numberFormat.CurrencyDecimalDigits);
-
-			//concatenate the full number format, e.g. #,0.00
-			var fullDigitFormat = $"#{numberFormat.CurrencyGroupSeparator}0{numberFormat.CurrencyDecimalSeparator}{decimalPlaces}";
-
-			//use string.Format on the patterns to get the positive and 
-			//negative formats
-			var positiveFormat = string.Format(positivePatternStrings[numberFormat.CurrencyPositivePattern],
-											   numberFormat.CurrencySymbol, fullDigitFormat);
-
-			var negativeFormat = string.Format(negativePatternStrings[numberFormat.CurrencyNegativePattern],
-											   numberFormat.CurrencySymbol, fullDigitFormat);
-
-			//finally, return the full format
-			return $"{positiveFormat};{negativeFormat}";
-		}
+			=> (currencySymbolRu == Currency);
 
 		public static bool operator ==(NumericParseResult obj1, NumericParseResult obj2)
 		{
